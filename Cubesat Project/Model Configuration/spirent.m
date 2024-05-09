2a9a0193f196e36d4f4f268a4c782e1c34efc649
sandraurbano <74840420+sandraurbano@users.noreply.github.com>
2024-05-09 10:59:10 +0200

    [FIX] spirent function

--- Cubesat Project/Model Configuration/spirent.m
+++ Cubesat Project/Model Configuration/spirent.m
@@ -1,4 +1,4 @@
-function spirent(mission,name)
+function spirent(mission,name,Resultspath)
 % Create a txt with simulator output data
 
 %% Process Data for Spirent
@@ -10,21 +10,14 @@ Data.Alt = mission.SimOutput.yout{7}.Values.Data;   %[m]
 Data.Vel = mission.SimOutput.yout{2}.Values.Data;   %[m/s]
 Data.PosECEF = mission.SimOutput.yout{1}.Values.Data;%[m]
 
-% T = table(Data.t, Data.Lat, Data.Lon , Data.Alt, Data.Vel(:,1), ...
-%     Data.Vel(:,2), Data.Vel(:,3), Data.Acc(:,1),Data.Acc(:,2),Data.Acc(:,3),...
-%     'VariableNames',{'t','lat','lon','alt','v1','v2','v3','a1','a2','a3'});
-% 
-% % Write the table to a CSV file
-% writetable(T,'Data/Cubesat_Alldata.txt','WriteMode','overwrite');
-
 MOT = repmat({'MOT'}, 1, length(Data.t));
 V1 = repmat({'v1_M1'}, 1, length(Data.t));
 
 T = table(Data.t,MOT', V1', Data.PosECEF(:,1), Data.PosECEF(:,2) , Data.PosECEF(:,3),...
     Data.Vel(:,1), Data.Vel(:,2), Data.Vel(:,3));
-% Write the table to a txt file
 
-file = ['Data/Cubesat_PosECEF_Vel_' name '.txt'];
+% Write the table to a txt file
+file = [Resultspath 'Cubesat_PosECEF_Vel_' name '.txt'];
 writetable(T,file,'WriteMode','overwrite');
 
 end
